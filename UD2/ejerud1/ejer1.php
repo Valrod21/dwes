9f0cce8029212ea669efc72c6066b8041e7b1343
Valrod21 <jval1819@gmail.com>
2021-09-27 18:30:35 +0100

    ejercicios 27/09/2021

--- UD2/ejerud1/ejer1.php
+++ UD2/ejerud1/ejer1.php
@@ -1,5 +1,11 @@
 <?php 
-$x = 144
-$y = 999
-
-echo .$x. ;
+$x = 144;
+$y = 999;
+$rest = $y - $x;
+$sum = $y + $x;
+echo "variable x= " . $x;
+echo "variable y= " . $y;
+echo "la resta  = " . $rest;
+echo "la suma   = " . $sum;
+echo "variable x= " . $x;
+echo "variable x= " . $x;
